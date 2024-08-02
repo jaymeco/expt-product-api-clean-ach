2f980c72f4dc65baf0d2ac5b3bef08b9cfcd35b8
jaymeco <jayme.anunciacao@gmail.com>
2024-08-02 11:46:20 -0300

    feature(domain/value-objects/sector-code): Adiciiona validação para o tamanho do código.

--- src/domain/value-objects/sector-code.ts
+++ src/domain/value-objects/sector-code.ts
@@ -1,8 +1,15 @@
+import { Rules } from "../../common/enums/rules";
+import InvalidArgumentException from "../exceptions/validation/invalid-argument";
+
 export class SectorCode {
   private constructor(private value: string) {
   }
 
   public static create(code: string) {
+    if (code.length > 2) {
+      throw new InvalidArgumentException('code', Rules.invalidSize);
+    }
+
     return new SectorCode(code);
   }
 
