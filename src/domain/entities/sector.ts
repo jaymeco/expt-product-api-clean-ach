b30b338af351c1397e3407abfc9ddf8545898489
jaymeco <jayme.anunciacao@gmail.com>
2024-08-01 21:24:50 -0300

    feature(domain/entities/sector): Implementa entidade de setor.

---
+++ src/domain/entities/sector.ts
@@ -0,0 +1,38 @@
+import { SectorCode } from "../value-objects/sector-code";
+import Uuid from "../value-objects/uuid";
+
+type Value = {
+  id: number | null;
+  uuid: Uuid;
+  code: SectorCode;
+  description: string;
+}
+
+export default class Sector {
+  private constructor(private value: Value) { }
+
+  public static create(code: string, description: string) {
+    return new Sector({
+      uuid: Uuid.create(),
+      code: SectorCode.create(code),
+      description,
+      id: null,
+    });
+  }
+
+  public getUuid() {
+    return this.value.uuid.getValue();
+  }
+
+  public get code() {
+    return this.value.code.getValue();
+  }
+
+  public get description() {
+    return this.value.description;
+  }
+
+  public isNew() {
+    return this.value.id === null;
+  }
+}
